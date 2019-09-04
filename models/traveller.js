0f55aa110e2a6e3edb4a95c0c50fe5998124f0f7
Jordan Smith <jordsmithy@gmail.com>
2019-09-04 19:07:22 +0100

    half of extension done

---
+++ models/traveller.js
@@ -0,0 +1,41 @@
+const Traveller = function(journeys) {
+  this.journeys = journeys;
+};
+
+Traveller.prototype.getJourneyStartLocations = function() {
+  const x = this.journeys.map((journey) => {
+    return journey.startLocation
+  })
+  return x
+};
+
+Traveller.prototype.getJourneyEndLocations = function () {
+  const x = this.journeys.map((journey) => {
+    return journey.endLocation
+  })
+  return x
+};
+
+Traveller.prototype.getJourneysByTransport = function (transport) {
+  const journeyResults = this.journeys.filter((journey) => journey.transport === transport)
+  return journeyResults
+};
+
+Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+  const journeyResults = this.journeys.filter((journey) => journey.distance >= minDistance)
+  return journeyResults
+};
+
+Traveller.prototype.calculateTotalDistanceTravelled = function () {
+  const total = this.journeys.reduce((runningTotal, journey) => {
+    return runningTotal + journey.distance
+  }, 0)
+  return total;
+};
+
+Traveller.prototype.getUniqueModesOfTransport = function () {
+
+};
+
+
+module.exports = Traveller;
